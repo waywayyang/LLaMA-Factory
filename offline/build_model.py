cf85276d362ccdfe708cf3d6c60c01a1cba46e91
waywayyang <yangwei.yw.wy@gmail.com>
2024-11-05 20:15:32 +0800

    修改模型结构

--- offline/build_model.py
+++ offline/build_model.py
@@ -1,19 +1,14 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer,AutoConfig,Qwen2TokenizerFast,Qwen2Tokenizer
-from transformers import Qwen2Model, Qwen2Config
-import yaml
-
 from llamafactory.model.way.configuration import WayConfig
 from llamafactory.model.way.model import WayModel
-from llamafactory.model.way.tokenizer import WayTokenizerFast
 config={
-    "vocab_size": 20000,
-    "hidden_size": 128,
+    "vocab_size": 151643,
+    "hidden_size": 512,
     "intermediate_size": 512,
     "num_hidden_layers": 16,
-    "num_attention_heads": 32,
-    "num_key_value_heads": 32,
+    "num_attention_heads": 16,
+    "num_key_value_heads": 16,
     "hidden_act": "silu",
-    "max_position_embeddings": 32768,
+    "max_position_embeddings": 512,
     "initializer_range": 0.02,
     "rms_norm_eps": 0.000001,
     "use_cache": True,
@@ -21,7 +16,7 @@ config={
     "rope_theta": 10000,
     "rope_scaling": None,
     "use_sliding_window": False,
-    "sliding_window": 1024,
+    "sliding_window": 131072,
     "max_window_layers": 28,
     "attention_dropout": 0
 }
